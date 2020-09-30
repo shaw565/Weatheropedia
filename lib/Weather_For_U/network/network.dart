8d75b1b2e46ff75bd398ce18f5c33fc236a6c8db
Soumendu Samanta <ss5481@bennett.edu.in>
2020-09-30 20:58:25 +0530

    First commit

---
+++ lib/Weather_For_U/network/network.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:soumendu/Weather_For_U/model/weather_forecast_model.dart';
+import 'package:soumendu/Weather_For_U/util/forecast_util.dart';
+import 'package:http/http.dart';
+
+class Network{
+  Future<WeatherForecastModel> getWeatherForecast({String cityName}) async{
+    var finalUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q="+cityName+
+        "&appid="+Util.appId+"&units=imperial";
+    final response = await get(Uri.encodeFull(finalUrl));
+    print("URL: ${Uri.encodeFull(finalUrl)}");
+
+    if (response.statusCode == 200) {
+      // we get the actual mapped model ( dart object )
+      print("weather data: ${response.body}");
+      return WeatherForecastModel.fromJson(json.decode(response.body));
+    }else {
+      throw Exception("Error getting weather forecast");
+    }
+
+     }
+}
